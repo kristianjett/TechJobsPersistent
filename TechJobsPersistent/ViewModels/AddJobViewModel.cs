43a7b36afbb7974e17cad4af0e9cbdb1fdd33d83
Kristian <jettkristian@gmail.com>
2020-12-09 00:12:17 -0800

    Connected to database, added "add employer" feature in pt. 2

---
+++ TechJobsPersistent/ViewModels/AddJobViewModel.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using TechJobsPersistent.Models;
+
+namespace TechJobsPersistent.ViewModels
+{
+    public class AddJobViewModel
+    {
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+        public int EmployerId { get; set; }
+        public List<SelectListItem> EmployerList { get; set; }
+
+        public AddJobViewModel() { }
+
+        public AddJobViewModel(List<Employer> possibleEmployers) : this()
+        {
+            EmployerList = new List<SelectListItem>();
+
+            foreach (Employer employer in possibleEmployers)
+            {
+                EmployerList.Add(new SelectListItem
+                {
+                    Value = employer.Id.ToString(),
+                    Text = employer.Name
+                });
+            }
+        }
+    }
+}
